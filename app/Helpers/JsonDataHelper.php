16616ab83a566b39f29a1b87c35a382e64592d1a
ZihongLin <1046298030@qq.com>
2018-01-24 13:30:23 +0800

    fuck you json_numeric

--- app/Helpers/JsonDataHelper.php
+++ app/Helpers/JsonDataHelper.php
@@ -12,9 +12,21 @@ if (!function_exists('JsonData')) {
 	function JsonData($data, $message = 'success', $status = 200)
 	{
 		return Response()->json([
-			'status'  => $status,
+			'status'  => strval($status),
 			'message' => $message,
-			'data'    => $data,
-		], $status, [], JSON_NUMERIC_CHECK);
+			'data'    => JSON_NUMERIC_STRING($data),
+		], $status, [], 0);
+//        return json_encode($data, 0);
 	}
+
+    function JSON_NUMERIC_STRING($array){
+        foreach($array as $key => $value){
+            if(is_array($value)){
+                $array[$key] = JSON_NUMERIC_STRING($value);
+            }elseif(is_numeric($value)){
+                $array[$key] = strval($value);
+            }
+        }
+        return $array;
+    }
 }
