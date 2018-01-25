3c87d7eab01dd3d721d6bebb9f227e53f60e142a
ZihongLin <1046298030@qq.com>
2018-01-25 16:07:00 +0800

    ticket changes

--- app/Http/Controllers/AdminVoteController.php
+++ app/Http/Controllers/AdminVoteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Answer;
 use App\Ticket;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AdminVoteController extends Controller
@@ -12,7 +13,7 @@ class AdminVoteController extends Controller
     function clearAllAnswers(Request $request)
     {
         Answer::all()->each(function ($item, $key) {
-           $item->delete();
+            $item->delete();
         });
     }
 
@@ -22,5 +23,19 @@ class AdminVoteController extends Controller
         Answer::where('source_type', 'ticket')->where('source_id', $ticket->id)->get()->each(function ($item, $key) {
             $item->delete();
         });
+        return JsonStatus('Done');
+    }
+
+    function operateTickets(Request $request)
+    {
+        $op = $request->route()[2]['route'];
+        $from = $request->route()[2]['from'];
+        $to = $request->route()[2]['to'];
+        for ($i = $from; $i <= $to; $i++) {
+            $ticket = Ticket::find($i);
+            $ticket->active = $op == 'enable' ? 1 : 0;
+            $ticket->save();
+        }
+        return JsonStatus('Done');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,7 @@ $router->group(['prefix' => 'vote'], function () use ($router) {
 $router->group(['prefix' => 'admin/fuckyvoty_secret_no_one_knows_gezilashichakeni'], function () use ($router) {
    $router->get('/clearAll', 'AdminVoteController@clearAllAnswers');
    $router->get('/clearTicket/{ticket}', 'AdminVoteController@clearTicketAnswers');
+   $router->get('ticket/{operation}/from/{from:[0-9]+}/to/{to:[0-9]+}', 'AdminVoteController@operateTickets');
 });
 
 $router->get('/testreq/{id}', 'ExampleController@testreq');
