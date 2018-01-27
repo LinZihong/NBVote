60d211bc1cea98999fedd165d8c9a2da1589f500
ZihongLin <1046298030@qq.com>
2018-01-27 17:32:43 +0800

    specify table

--- app/MicOptionCache.php
+++ app/MicOptionCache.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class MicOptionCache extends Model
 {
     //
+    protected  $table = 'mic_option_cache';
     protected $fillable = ['options', 'ticket_string', 'vote_id'];
     protected $casts = [
         'options' => 'array'
